f4cb5bebafef34ef4b84bd32c226dd6028d7e945
rathorenikita <nr9479451@gmail.com>
2024-08-22 08:46:45 +0000

    numbers and math

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,48 @@
+const score = 400
+console.log(score);
+const newscore = new Number(score)
+// console.log(newscore);
+// console.log(newscore.toString().length);
+// console.log(newscore.toFixed(2));
+const otherNumber = 12.89455
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ***********************Maths*******************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(5.2));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(3,6,2,4));
+// console.log(Math.max(3,6,4,2));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
