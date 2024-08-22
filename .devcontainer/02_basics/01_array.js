4e20b209dfaa8b0896b2970694140235dccdf667
rathorenikita <nr9479451@gmail.com>
2024-08-22 13:38:49 +0000

    array

---
+++ .devcontainer/02_basics/01_array.js
@@ -0,0 +1,55 @@
+const myArray = [1,2,3,4] 
+const myHeroes = ["shaktiman","nagraj"]
+//console.log(myArray[0]);
+
+const myArray2 = new Array(1,2,3,4)
+//console.log(myArray2[1]);
+
+/***************array method */
+
+//myArray.push(7)
+//console.log(myArray);
+//myArray.push(9)
+//console.log(myArray);
+//myArray.pop()
+//console.log(myArray);
+//myArray.unshift(10)
+//console.log(myArray);
+//myArray.shift()
+//console.log(myArray);
+
+// console.log(myArray.includes(4));
+// console.log(myArray.includes(7));
+// console.log(myArray.indexOf(1));
+// console.log(myArray.indexOf(7));
+
+const newArr = myArray.join()
+// console.log(myArray);
+// console.log(newArr);
+
+// console.log(typeof newArr);
+console.log("A", myArray);
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B", myArray);
+
+const myn2= myArray.splice(1,3)
+console.log(myn2);
+
+console.log("C",myArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
