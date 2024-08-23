97051d4415bf6f6cd609f061d7852fba29e87bc2
rathorenikita <nr9479451@gmail.com>
2024-08-23 07:50:35 +0000

    function

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,33 @@
+function myName (){
+    console.log("n");
+    console.log("i");
+    console.log("k");
+    console.log("i");
+    console.log("t");
+    console.log("a");
+    
+
+}
+myName()
+
+// function addNum (number1,number2){
+//    console.log(number1 + number2);
+    
+// }
+//addNum(3, "a")
+
+function addNum (number1,number2){
+    // let result = (number1 + number2);
+    // return result
+    return number1+number2
+     
+ }
+  result =addNum(5,6)
+ //console.log(result);
+
+ function loginUser (username){
+    return `just ${username} logged in`
+ }
+ console.log(loginUser("nimish"));
+  
+ 
