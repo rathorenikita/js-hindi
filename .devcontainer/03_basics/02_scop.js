44ab1fec2cfd66c55bdb403c871eb969b51974ba
rathorenikita <nr9479451@gmail.com>
2024-08-24 05:40:10 +0000

    scope

---
+++ .devcontainer/03_basics/02_scop.js
@@ -0,0 +1,54 @@
+//const a =100
+ //let b =200
+//var c = 300
+
+//(true)
+{
+    const a =10
+    let b =20
+    var c = 30
+   // console.log(a);
+    
+}
+//console.log(a)
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const name = "nikita"
+
+    function two()
+    {
+        const website = "youtube"
+        //console.log(name);
+        
+    }
+   // two()
+}
+//one()
+
+if (true)
+{
+    const username = "nikita"
+    if(username == "nikita")
+    {
+        const website = " youtube"
+       // console.log(username + website);
+        
+    }
+   // console.log(website);
+    
+}
+//console.log(username);
+console.log(addOne(5))
+function addOne(num){
+    return num+1
+}
+addOne(5)
+
+const addTwo = function addTwo(num)
+{
+    return num +2
+}
+addTwo(6)
+
