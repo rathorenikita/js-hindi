a9b652eea3cdfef325591be81feb6622295ea2f0
rathorenikita <nr9479451@gmail.com>
2024-08-25 06:10:08 +0000

    arrows

---
+++ .devcontainer/03_basics/03_arrows.js
@@ -0,0 +1,23 @@
+const user = {
+    username: "nikita",
+    price: 999,
+
+    welcomeMessage: function(){
+    //console.log(`${this.username},welcome to website`);
+   // console.log(this);
+    
+
+}
+}
+// user.welcomeMessage()
+//  user.username= "sam"
+//  user.welcomeMessage()
+
+//const addTwo = (num1,num2) =>{
+    //return num1 + num2
+//}
+//console.log(addTwo(3,4));
+
+const addTwo =(num1,num2) => ({username: "nikita"})
+console.log(addTwo());
+
