324a53e97530b3488091c8d03dbd855f292bb555
rathorenikita <nr9479451@gmail.com>
2024-08-25 06:10:33 +0000

    switch

---
+++ .devcontainer/04_cantrol_flow/switch.js
@@ -0,0 +1,18 @@
+const month = 2
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        break;
+}
