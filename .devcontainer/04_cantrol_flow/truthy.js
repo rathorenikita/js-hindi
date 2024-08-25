59efcf041feda56a62edc7f791b8946e48a1bbf6
rathorenikita <nr9479451@gmail.com>
2024-08-25 06:10:48 +0000

    truthy

---
+++ .devcontainer/04_cantrol_flow/truthy.js
@@ -0,0 +1,16 @@
+const username = " "
+if (username) {
+   // console.log("got user email");
+    
+}
+else
+{
+    //console.log("dont have user email");
+    
+}
+let val1 ;
+val1 = null??10
+//console.log(val1);
+const iceCream =100
+iceCream>80? console.log("morethan 80"): console.log("less than 80");
+
