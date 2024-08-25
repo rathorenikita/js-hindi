db24dd1ba8906d21ab621e0230008cef26b039c6
rathorenikita <nr9479451@gmail.com>
2024-08-25 06:11:21 +0000

    Ifelse

---
+++ .devcontainer/04_cantrol_flow/one.js
@@ -0,0 +1,22 @@
+const score  = 200
+if(score>100)
+{
+    const power = "fly"
+    //console.log(`user power ${power}`);
+    
+}
+
+const userLoggenIn = true
+const debitCard = true
+const logInFromGoogle = true
+const logTnFromEmail = false
+if (userLoggenIn && debitCard) 
+    {
+    //console.log("allow to shopping");
+    
+}
+if (logInFromGoogle || logTnFromEmail) 
+    {
+    console.log("log in successfully");
+    
+}
